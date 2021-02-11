8c822737ac4c87ed55859f5dccb432f858c7f06c
Caroline Sih <sih.caroline@gmail.com>
2021-02-11 00:44:40 -0800

    completed fb and ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,16 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, callBackFn)
+}
+
+function callBackFn(result) {
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	  '<img src="' + result['image'] + '" class="detailsImage">' +
+	  '<p>' + result['title'] + '</p>' +
+	  '<p><small>' + result['date'] +
+	  '</small></p></a>' +
+	  '<p>' + result['summary'] + '</p>';
+
+	$('#project' + result.id).find('.thumbnail').find('.details').html(projectHTML);
 }
